c6c4381f6ddc4c68c605088f599f76ee20c766fb
Gabriel Robitaille-Montpetit <grmontpetit@inocybe.com>
2014-09-18 12:55:52 -0400

    Added the implementation of cup_provider.

--- provider/src/main/java/org/opendaylight/controller/config/yang/config/cup_provider/impl/CupProviderModule.java
+++ provider/src/main/java/org/opendaylight/controller/config/yang/config/cup_provider/impl/CupProviderModule.java
@@ -1,4 +1,8 @@
 package org.opendaylight.controller.config.yang.config.cup_provider.impl;
+
+import org.opendaylight.controller.OpendaylightCup;
+import org.opendaylight.controller.md.sal.binding.api.DataBroker;
+
 public class CupProviderModule extends org.opendaylight.controller.config.yang.config.cup_provider.impl.AbstractCupProviderModule {
     public CupProviderModule(org.opendaylight.controller.config.api.ModuleIdentifier identifier, org.opendaylight.controller.config.api.DependencyResolver dependencyResolver) {
         super(identifier, dependencyResolver);
@@ -16,7 +20,23 @@ public class CupProviderModule extends org.opendaylight.controller.config.yang.c
     @Override
     public java.lang.AutoCloseable createInstance() {
         // TODO:implement
-        throw new java.lang.UnsupportedOperationException();
+        final OpendaylightCup opendaylightCup = new OpendaylightCup();
+        
+        DataBroker dataBrokerService = getDataBrokerDependency();
+        opendaylightCup.setDataProvider(dataBrokerService);
+        
+        // Wrap cup as AutoCloseable and close registrations to md-sal at
+        // close(). The close method is where you would generally clean up thread pools
+        // etc.
+        final class AutoCloseableCup implements AutoCloseable {
+
+            @Override
+            public void close() throws Exception {
+                opendaylightCup.close();
+            }
+        }
+        
+        return new AutoCloseableCup();
     }
 
 }
