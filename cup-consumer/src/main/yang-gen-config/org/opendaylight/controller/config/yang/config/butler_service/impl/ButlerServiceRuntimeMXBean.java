d5e44f0652294a2e3645af6cef73fb61ad98b3e8
Gabriel Robitaille-Montpetit <grmontpetit@inocybe.com>
2015-03-26 23:07:22 -0400

    Updated parent pom file with newest versions. Distribution sub project is compiling, but not getting the cup feature correctly. Modified the features file.

--- cup-consumer/src/main/yang-gen-config/org/opendaylight/controller/config/yang/config/butler_service/impl/ButlerServiceRuntimeMXBean.java
+++
@@ -1,5 +0,0 @@
-package org.opendaylight.controller.config.yang.config.butler_service.impl;
-public interface ButlerServiceRuntimeMXBean extends org.opendaylight.controller.config.api.runtime.RuntimeBean {
-    public java.lang.Boolean makeBlackTeaWithLapresse();
-
-}
--- cup-provider/src/main/yang-gen-config/org/opendaylight/controller/config/yang/config/cup_provider/impl/CupProviderRuntimeMXBean.java
+++
@@ -1,7 +0,0 @@
-package org.opendaylight.controller.config.yang.config.cup_provider.impl;
-public interface CupProviderRuntimeMXBean extends org.opendaylight.controller.config.api.runtime.RuntimeBean {
-    public java.lang.Long getCupsMade();
-
-    public void clearCupsMade();
-
-}
